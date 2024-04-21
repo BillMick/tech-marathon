b7d68e4b31bbb9f473d89e21fbc020520d7d7bad
BillMick <mikeahoudjinou@gmail.com>
2024-04-21 04:54:40 +0100

    Basic Syntax.

--- Learn the basics/basic_syntax.py
+++ Learn the basics/basic_syntax.py
@@ -1,1 +1,32 @@
+# # First Python Program
+# # Python - Script Mode Programming
 print ("Hello, World!")
+# Execute whereis command to know python path
+# !/usr/bin/python 
+
+# # Python Identifiers
+# A Python identifier is a name used to identify a variable, function, class, module or other object.
+# Python Class names start with an uppercase letter. All other identifiers start with a lowercase letter.
+# Starting an identifier with a single leading underscore indicates that the identifier is private identifier.
+# Starting an identifier with two leading underscores indicates a strongly private identifier.
+# If the identifier also ends with two trailing underscores, the identifier is a language-defined special name.
+
+# # Python Multi-Line Statements
+total = "item_one" + \
+        "item_two" + \
+        "item_three"
+print(total)
+
+# # Quotations in Python
+word = 'word'
+print (word)
+
+sentence = "This is a sentence."
+print (sentence)
+
+paragraph = """This is a paragraph. It is
+ made up of multiple lines and sentences."""
+print (paragraph)
+
+# # Waiting for the User
+a = input("\n\nPress the enter key to exit.")
