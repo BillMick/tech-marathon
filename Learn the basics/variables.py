4dde78fcf127e469d9a922c4690dcaa41c10fb03
BillMick <mikeahoudjinou@gmail.com>
2024-04-21 05:40:03 +0100

    Variables.

--- Learn the basics/variables.py
+++ Learn the basics/variables.py
@@ -38,3 +38,20 @@ print(f"b = {b}, c = {c}, d = {d}")
 # Python doesn't have any formally defined constants, However you can indicate a variable to be treated 
 # as a constant by using all-caps names with underscores. For example, the name PI_VALUE indicates that 
 # you don't want the variable redefined or changed in any way.
+
+# # Python vs C/C++ Variables
+# The concept of variable works differently in Python than in C/C++. In C/C++, a variable is a named memory location. If a=10 and also b=10, both are two different memory locations. Let us assume their memory address is 100 and 200 respectively.
+# If a different value is assigned to "a" - say 50, 10 in the address 100 is overwritten.
+# A Python variable refers to the object and not the memory location. An object is stored in 
+# memory only once. Multiple variables are really the multiple labels to the same object.
+b = 3
+print(f"id(b) = {id(b)}, id(c) = {id(c)}")
+# The statement a=50 creates a new int object 50 in the memory at some other location, leaving the object 10 referred by "b".
+# Further, if you assign some other value to b, the object 10 remains unreferred.
+# Python's garbage collector mechanism releases the memory occupied by any unreferred object.
+
+# Python's identity operator "is" returns True if both the operands have same id() value.
+a = b = 10
+print(f"a is b ?= {a is b}")
+
+print(f"id(a) = {id(a)}, id(b) = {id(b)}")
