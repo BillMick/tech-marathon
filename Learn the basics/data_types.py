7f0dcdcc25e845b3deafd41a46e041cf54b949c5
BillMick <mikeahoudjinou@gmail.com>
2024-04-22 06:36:37 +0100

    Variables.

--- Learn the basics/data_types.py
+++ Learn the basics/data_types.py
@@ -7,12 +7,12 @@
 # matter. Hence, following string representations are equivalent.
 
 # # Example of String Data Type
-str = 'Hello World!'
+str = 'Hello World! '
 print (str)          # Prints complete string
 print (str[0])       # Prints first character of the string
 print (str[2:5])     # Prints characters starting from 3rd to 5th
 print (str[2:])      # Prints string starting from 3rd character
-print (str * 5)      # Prints string two times
+print (str * 5)      # Prints string fives times
 print (str + "TEST") # Prints concatenated string
 
 # # Python List Data Type
@@ -20,3 +20,19 @@ print (str + "TEST") # Prints concatenated string
 # can also be an item in another list. In that case, it becomes a nested list.
 nested_list = [['One', 'Two', 'Three'], [1,2,3], [1.0, 2.0, 3.0]]
 print(f"nested_list = {nested_list}")
+
+# # Example of List Data Type
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+print(list)            # Prints complete list
+print(list[0])         # Prints first element of the list
+print(list[1:3])       # Prints elements starting from 2nd till 3rd 
+print(list[2:])        # Prints elements starting from 3rd element
+print(tinylist * 2)    # Prints list two times
+print(list + tinylist) # Prints concatenated lists
+
+# # Python Tuple Data Type
+# To form a tuple, use of parentheses is optional. Data items 
+# separated by comma without any enclosing symbols are treated as a tuple by default.
+test = 15, 17, "a"
+print(f"test_type = {type(test)}")
