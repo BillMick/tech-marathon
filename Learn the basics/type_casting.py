b2719adaee8bf21adc0f69706439c47e8b52d788
BillMick <mikeahoudjinou@gmail.com>
2024-04-23 23:24:39 +0100

    Type casting.

--- Learn the basics/type_casting.py
+++ Learn the basics/type_casting.py
@@ -5,7 +5,7 @@
 # before the addition with a floating point object. In the following example, 
 # we try to add a Boolean object in a float, pleae note that True is equal to 
 # 1, and False is equal to 0.
-
+print("Python Implicit Casting")
 a = True
 b = 10.5
 c = a + b
@@ -15,3 +15,48 @@ print (c)
 # Although automatic or implicit casting is limited to int to float conversion,
 # you can use Python's built-in functions int(), float() and str() to perform 
 # the explicit conversions
+
+# Binary String to Integer
+# The string should be made up of 1 and 0 only, and the base should be 2.
+a = int("110011", 2)
+print("Binary String", f"a = {a}", sep="\n")
+
+# Octal String to Integer
+# The string should only contain 0 to 7 digits, and the base should be 8.
+a = int("20", 8)
+print("Octal String", f"a = {a}", sep="\n")
+
+# Hexa-Decimal String to Integer
+# The string should contain only the Hexadecimal symbols i.e., 0-9 and A, B, C, D, E or F.
+# Base should be 16.
+a = int("2A9", 16)
+print("Hexa-Decimal String", f"a = {a}", sep="\n")
+
+# # Conversion of Sequence Types
+print("Conversion of Sequence Types")
+a = [1,2,3,4,5]   # List Object
+b = (1,2,3,4,5)   # Tupple Object
+c = "Hello"       # String Object
+
+### list() separates each character in the string and builds the list
+obj = list(c)
+print("list() separates each character in the string and builds the list", obj, sep="\n")
+
+### The parentheses of tuple are replaced by square brackets.
+obj=list(b)
+print("The parentheses of tuple are replaced by square brackets.", obj, sep="\n")
+
+### tuple() separates each character from string and builds a tuple of characters.
+obj=tuple(c)
+print("tuple() separates each character from string and builds a tuple of characters.", obj, sep="\n")
+
+### square brackets of list are replaced by parentheses.
+obj=tuple(a)
+print("square brackets of list are replaced by parentheses.", obj, sep="\n")
+
+### str() function puts the list and tuple inside the quote symbols.
+obj=str(a)
+print("str() function puts the list inside the quote symbols.", type(obj), sep="\n")
+
+obj=str(b)
+print("str() function puts the tuple inside the quote symbols.", type(obj), sep="\n")
