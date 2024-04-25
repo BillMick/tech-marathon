37a34f932745dfc4cd89721eb1c14cdda9f79a2f
BillMick <mikeahoudjinou@gmail.com>
2024-04-25 20:55:35 +0100

    Exceptions.

--- Learn the basics/errors_exceptions.py
+++ Learn the basics/errors_exceptions.py
@@ -108,7 +108,7 @@ class SalaryNotInRangeError(Exception):
         salary -- input salary which caused the error
         message -- explanation of the error
     """
-    def __init__(self, salary, message="Salary is not in (5000, 15000) range"):
+    def __init__(self, salary, message = "Salary is not in (5000, 15000) range"):
         self.salary = salary
         self.message = message
         super().__init__(self.message)
