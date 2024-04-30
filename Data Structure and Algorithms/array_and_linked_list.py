5cfcbe6b03180037fa1733cc1af56ca2ec4b79cd
BillMick <mikeahoudjinou@gmail.com>
2024-04-30 05:42:35 +0100

    Arrays and Linked Lists.

---
+++ Data Structure and Algorithms/array_and_linked_list.py
@@ -0,0 +1,24 @@
+# # Arrays and Linked lists
+
+# # Arrays
+# Arrays store elements in contiguous memory locations, resulting in easily
+# calculable addresses for the elements stored and this allows faster access 
+# to an element at a specific index.
+# As mentioned in the section above, arrays store only items that are of the 
+# same single data type.
+# Do not misunderstand arrays and lists. Lists store items that are of various data types. 
+# Lists are built into the Python programming language, whereas arrays aren't. 
+# Arrays are not a built-in data structure, and therefore need to be imported via the 
+# array module in order to be used.
+# They are also more compact and take up less memory and space which makes 
+# them more size efficient compared to lists.
+# To import : import array
+# Syntax : variable_name = array(typecode,[elements])
+import array as arr 
+numbers = arr.array('i',[10,20,30])
+print(numbers)
+
+# # Linked lists
+# Linked lists are less rigid in their storage structure and elements are 
+# usually not stored in contiguous locations, hence they need to be stored
+# with additional tags giving a reference to the next element. 
