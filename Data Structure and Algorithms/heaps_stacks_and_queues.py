34ef6ab204702c298d92778358c869c4864c3eda
BillMick <mikeahoudjinou@gmail.com>
2024-05-04 07:56:49 +0100

    Heaps Stacks and Queues.

---
+++ Data Structure and Algorithms/heaps_stacks_and_queues.py
@@ -0,0 +1,10 @@
+# # Heaps Stacks and Queues
+print("""The purpose of a min-heap is to store objects that have a partial
+      order on them. It has a fast (O(log n)) method for removing the 
+      minimum object from the heap. Min-heaps are useful for calculations 
+      where you have multiple minimum computations to perform.\nThere is 
+      an analogous structure called a max-heap, which extracts the maximum 
+      value from the heap.\nA heap is either a max-heap or a min-heap - 
+      it can't be both. Given an implementation of a min-heap, a max-heap 
+      can be implemented by reversing the comparison between elements.""")
+
