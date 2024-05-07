ba14d2f53e0b69cabc4874991e5919543077aa79
BillMick <mikeahoudjinou@gmail.com>
2024-05-07 06:53:34 +0100

    Stacks.

--- Data Structure and Algorithms/stacks.py
+++ Data Structure and Algorithms/stacks.py
@@ -9,6 +9,22 @@ print("""The functions associated with stacks are :\n· empty() – Returns whet
 \n· push(a) – Inserts the element at the top of the stack – Time Complexity: O(1)
 \n· pop() – Deletes the topmost element of the stack – Time Complexity: O(1)""")
 
+print("""Advantages of Stack:\n
+· Stacks are simple data structures with a well-defined set of operations, 
+which makes them easy to understand and use.\n
+· Stacks are efficient for adding and removing elements, as these operations 
+have a time complexity of O(1).\n
+· In order to reverse the order of elements we use the stack data structure.\n
+· Stacks can be used to implement undo/redo functions in applications.""")
+
+print("""Drawbacks of Stack:\n
+· Restriction of size in Stack is a drawback and if they are full, you cannot 
+add any more elements to the stack.\n
+· Stacks do not provide fast access to elements other than the top element.\n
+· Stacks do not support efficient searching, as you have to pop elements one 
+by one until you find the element you are looking for.""")
+
+# # Implementation using list
 class Stack:
     stack = []
     
@@ -50,3 +66,85 @@ print("Is empty: ", my_stack.empty())
 print(my_stack.pop())
 print("Last element (in the top): ", my_stack.top())
 print("First element (in the bottom): ", my_stack.bottom())
+
+# # Implementation using deque
+print("""Python stack can be implemented using the deque class from the 
+      collections module. Deque is preferred over the list in the cases 
+      where we need quicker append and pop operations from both the ends of
+      the container, as deque provides an O(1) time complexity for append 
+      and pop operations as compared to list which provides O(n) time
+      complexity. """)
+
+class StackDeque:
+    
+    stack = None
+    
+    def __init__(self) -> None:
+        from collections import deque
+        print("Initializing...")
+        self.stack = deque()
+        print(self.stack)
+    
+    def push(self, element):
+        self.stack.append(element)
+        return self.stack
+    
+    def pop(self):
+        self.stack.pop()
+        return self.stack
+
+my_deque_stack = StackDeque
+my_deque_stack.push(element='a')
+my_deque_stack.push('b')
+my_deque_stack.push('c')
+print('Initial stack:')
+print(my_deque_stack.stack)
+print('\nElements popped from stack:')
+print(my_deque_stack.pop())
+print(my_deque_stack.pop())
+print(my_deque_stack.pop())
+print('\nStack after elements are popped:')
+print(my_deque_stack.stack)
+
+# uncommenting print(stack.pop())
+# will cause an IndexError
+# as the stack is now empty
+
+
+# Python program to
+# demonstrate stack implementation
+# using queue module
+
+# # Implementation using queue module
+print("""Queue module also has a LIFO Queue, which is basically a Stack.
+      Data is inserted into Queue using the put() function and get() takes 
+      data out from the Queue.""")
+
+from queue import LifoQueue
+
+# Initializing a stack
+stack = LifoQueue(maxsize=3)
+
+# qsize() show the number of elements
+# in the stack
+print(stack.qsize())
+
+# put() function to push
+# element in the stack
+stack.put('a')
+stack.put('b')
+stack.put('c')
+
+print("Full: ", stack.full())
+print("Size: ", stack.qsize())
+
+# get() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from the stack')
+print(stack.get())
+print(stack.get())
+print(stack.get())
+
+print("\nEmpty: ", stack.empty())
+
