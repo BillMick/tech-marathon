447f67e5d57a072bb99f5a5fcde263b191b9b9cc
BillMick <mikeahoudjinou@gmail.com>
2024-05-10 04:21:59 +0100

    Hash tables.

--- Data Structure and Algorithms/hash_tables.py
+++
@@ -1,4 +0,0 @@
-print("""Hash Table, Map, HashMap, Dictionary or Associative are all the names
-      of the same data structure. It is a data structure that implements a 
-      set abstract data type, a structure that can map keys to values.""")
-
---
+++ Data Structure and Algorithms/hash_tables_hashmap.py
@@ -0,0 +1,11 @@
+print("""Hash Table, Map, HashMap, Dictionary or Associative are all the names
+      of the same data structure. It is a data structure that implements a 
+      set abstract data type, a structure that can map keys to values.""")
+
+print("""In computer science, a Hash table or a Hashmap is a type of data
+      structure that maps keys to its value pairs (implement abstract array
+      data types). It basically makes use of a function that computes an
+      index value that in turn holds the elements to be searched, inserted,
+      removed, etc. This makes it easy and fast to access data. In general,
+      hash tables store key-value pairs and the key is generated using a 
+      hash function.""")
