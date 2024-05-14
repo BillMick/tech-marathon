7814a65aaf98a04c510237f93563a20d57a5ea2a
BillMick <mikeahoudjinou@gmail.com>
2024-05-14 04:37:16 +0100

    Binary Search Trees.

--- Data Structure and Algorithms/binary_search_tree.py
+++ Data Structure and Algorithms/binary_search_tree.py
@@ -10,3 +10,9 @@ print("""Binary trees are useful for storing data in an organized manner so that
       linear O(n) time required to find elements in an unsorted array. Many popular 
       production databases such as PostgreSQL and MySQL use binary trees under the 
       hood to speed up CRUD operations.""")
+print("""CONS OF A BST
+· Slow for a brute-force search. If you need to iterate over each node, you might have 
+more success with an array.
+· When the tree becomes unbalanced, all fast O(log(n)) operations quickly degrade to O(n).
+· Since pointers to whole objects are typically involved, a BST can require quite a bit 
+more memory than an array, although this depends on the implementation.""")
