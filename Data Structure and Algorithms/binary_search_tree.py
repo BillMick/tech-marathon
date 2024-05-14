33c42a2b424eddb60c23b6b2f4772df2fc8b718f
BillMick <mikeahoudjinou@gmail.com>
2024-05-14 05:07:26 +0100

    Binary Search Trees.

--- Data Structure and Algorithms/binary_search_tree.py
+++ Data Structure and Algorithms/binary_search_tree.py
@@ -16,3 +16,139 @@ more success with an array.
 · When the tree becomes unbalanced, all fast O(log(n)) operations quickly degrade to O(n).
 · Since pointers to whole objects are typically involved, a BST can require quite a bit 
 more memory than an array, although this depends on the implementation.""")
+
+print('IMPLEMENTING A B-TREE IN PYTHON')
+# print('STEP 1 - BSTNODE CLASS')
+print("""Our implementation won’t use a Tree class, but instead just a Node class. Binary 
+      trees are really just a pointer to a root node that in turn connects to each child 
+      node, so we’ll run with that idea.""")
+class BSTNode:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+    
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+        
+    def get_min(self):
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current.val
+
+    def get_max(self):
+        current = self
+        while current.right is not None:
+            current = current.right
+        return current.val
+    
+    def exists(self, val):
+        if val == self.val:
+            return True
+        if val < self.val:
+            if self.left == None:
+                return False
+            return self.left.exists(val)
+        if self.right == None:
+            return False
+        return self.right.exists(val)
+    
+    def delete(self, val):
+        if self == None:
+            return self
+        if self.right == None:
+            return self.left
+        if self.left == None:
+            return self.right
+        if val < self.val:
+            if self.left:
+                self.left = self.left.delete(val)
+            return self
+        if val > self.val:
+            if self.right:
+                self.right = self.right.delete(val)
+            return self
+        min_larger_node = self.right
+        while min_larger_node.left:
+            min_larger_node = min_larger_node.left
+        self.val = min_larger_node.val
+        self.right = self.right.delete(min_larger_node.val)
+        return self
+
+    def inorder(self, vals):
+        if self.left is not None:
+            self.left.inorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        if self.right is not None:
+            self.right.inorder(vals)
+        return vals
+
+    def preorder(self, vals):
+        if self.val is not None:
+            vals.append(self.val)
+        if self.left is not None:
+            self.left.preorder(vals)
+        if self.right is not None:
+            self.right.preorder(vals)
+        return vals
+
+    def postorder(self, vals):
+        if self.left is not None:
+            self.left.postorder(vals)
+        if self.right is not None:
+            self.right.postorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        return vals
+
+def main():
+    nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+    bst = BSTNode()
+    for num in nums:
+        bst.insert(num)
+    print("preorder:")
+    print(bst.preorder([]))
+    print("#")
+    print("postorder:")
+    print(bst.postorder([]))
+    print("#")
+    print("inorder:")
+    print(bst.inorder([]))
+    print("#")
+    nums = [2, 6, 20]
+    print("deleting " + str(nums))
+    for num in nums:
+        bst.delete(num)
+    print("#")
+    print("4 exists:")
+    print(bst.exists(4))
+    print("6 exists:")
+    print(bst.exists(6))
+    print("12 exists:")
+    print(bst.exists(12))
+    print("18 exists:")
+    print(bst.exists(18))
+    print("#")
+    print("inorder:")
+    print(bst.inorder([]))
+
+main()
