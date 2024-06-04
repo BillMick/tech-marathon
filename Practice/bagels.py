030a9eef3d23b395d6f7186f69ae655e2e3c0e34
BillMick <mikeahoudjinou@gmail.com>
2024-06-04 05:56:46 +0100

    Bagels - reformat number.

--- Practice/bagels.py
+++ Practice/bagels.py
@@ -4,8 +4,16 @@ def random_number()->str:
     """
     import random
     magic_number = str(random.randint(0, 999))
+    magic_number = reformat_number(magic_number)
     return magic_number
 
+def reformat_number(number:str)->str:
+    match len(number):
+        case 1:
+            number = '00' + number
+        case 2:
+            number = '0' + number
+    return number
         
 
 number = random_number()
