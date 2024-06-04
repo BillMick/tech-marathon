4a20853059cce528188ef7bdaa28ec715f407cc3
BillMick <mikeahoudjinou@gmail.com>
2024-06-04 20:04:49 +0100

    Bagels - Doc + Misc.

--- Practice/bagels.py
+++ Practice/bagels.py
@@ -2,9 +2,9 @@ def random_number()->str:
     """
     Generator of a three digits number between 000 and 999.
     """
-    import random
-    magic_number = random.randint(0, 999)
-    magic_number = reformat_number(magic_number)
+    from random import randint
+    magic_number = randint(0, 999) # generate integer between 0 and 999
+    magic_number = reformat_number(magic_number) # reformat the integer to have 3-digits
     return magic_number
 
 def reformat_number(number:int)->str:
@@ -12,12 +12,12 @@ def reformat_number(number:int)->str:
     As we need a three digits number, zeros are completed to the left of
     the given number if len(number) != 3.
     """
-    number = str(number)
+    number = str(number) # to able string operations
     match len(number):
-        case 1:
-            number = '00' + number
-        case 2:
-            number = '0' + number
+        case 1: # len(number) == 1
+            number = '00' + number # add two 0
+        case 2: # len(number) == 2
+            number = '0' + number # add one 0
     return number
 
 def number_guessing()->str:
@@ -26,45 +26,46 @@ def number_guessing()->str:
     As we need a three digits number, zeros are completed to the left of
     user entry if len(user_entry) != 3."""
     try:
-        user_entry = int(input("Guess the number: "))
-        user_entry = reformat_number(user_entry)
-        assert 0 < len(user_entry) < 4
+        user_entry = int(input("Guess the number: ")) # get user entry
+        user_entry = reformat_number(user_entry) # reformat the entry to have 3-digits
+        assert len(user_entry) == 3 # assertion: entry length must equal 3.
         return user_entry
-    except (TypeError, ValueError, AssertionError):
+    except (TypeError, ValueError, AssertionError): # error handling
         print("Invalide input. Try again.")
-        user_entry = number_guessing()
+        user_entry = number_guessing() # recall the function
         return user_entry
 
 def comparison(magic_number:str, user_entry:str)->str:
     """
     Comparison of magic_number and user entry.
     """
-    pico, fermi = [], []
+    pico, fermi = [], [] # initialization
     for i in range(0, len(user_entry)):
         # if user_entry[i] in magic_number: pico.append(True); pico.append(False)
         # if user_entry[i] == magic_number[i]: fermi.append(True); fermi.append(False)
-        if user_entry[i] in magic_number: 
+        if user_entry[i] in magic_number: # be able to know at which position and how many 'pico' we have
             pico.append(True)
         else:
             pico.append(False)
-        if user_entry[i] == magic_number[i]: 
+        if user_entry[i] == magic_number[i]: # be able to know at which position and how many 'fermi' we have
             fermi.append(True)
         else:
             fermi.append(False)
-    if True not in pico and True not in fermi:
+    if True not in pico and True not in fermi: # no pico neither fermi
         return 'Bagels '
-    elif fermi == [True, True, True]:
+    elif fermi == [True, True, True]: # 3 fermi => correct number
         return 'Correct'
-    elif True in fermi:
+    elif True in fermi: # at least one fermi
         return 'Fermi  '
-    elif True in pico:
+    elif True in pico: # at least one pico
         return 'Pico   '
 
 def stats(magic_number:str, guessed_numbers_list:list, comparison_results_list:list)->None:
     """
     Bagels game stats printing.
     """
-    rounds = len(comparison_results_list)
+    rounds = len(comparison_results_list) # get the length of the table to draw.
+    # Stats table drawing
     print(f"""
               {'-'*65}
               ### Guess n°  || Magic Number || Guessed Number || Decision   ###
@@ -83,6 +84,7 @@ def stats(magic_number:str, guessed_numbers_list:list, comparison_results_list:l
 def main():
     """
     Bagels game main function."""
+    # Informations about the game
     print(f"""
     {'#'*75}
     ### Bagels, a deductive logic game.                                     ###
@@ -98,24 +100,24 @@ def main():
     ### You have 10 guesses to get it.                                      ### 
     {'#'*75} 
     """)
-    while True:
+    while True: # be able to play lots of rounds
         number_of_guesses = 10 # number of attempts
         guessed_numbers_list, comparison_results_list = [], [] # initialization
         magic_number = random_number() # call for the 3-digits number generator
         # Loop to process the number of attempts
         for i in range(0, number_of_guesses):
             print(f'Guess #{i + 1}:')
-            guessed_number = number_guessing() 
+            guessed_number = number_guessing() # get user number
             guessed_numbers_list.append(guessed_number) # for stats need
             comparison_result = comparison(magic_number, guessed_number) # Comparison making
             comparison_results_list.append(comparison_result) # for stats need
-            print(comparison_result)
-            if comparison_result == 'Correct':
+            print(comparison_result) # tell if we have bagels, pico, fermi or correct
+            if comparison_result == 'Correct': # when number found
                 print('Congrats !!! You have found the number. See your stats below.')
-                break
-            elif i == 9:
-                print('Number not found...', f'It was {magic_number}.')
-        # Game stats.
+                break # end the round
+            elif i == number_of_guesses - 1: # no more attempt
+                print('Number not found...', f'It was {magic_number}.') # give the answer anyway
+        # Show stats.
         stats(magic_number, guessed_numbers_list, comparison_results_list)
         # Ask for want of playing another round.
         while 1:
@@ -129,4 +131,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
