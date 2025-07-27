cfccbb3cef95cfba25fc546916fd5fa5012078c3
BillMick <ahouandjinoubill@gmail.com>
2025-07-27 23:35:12 +0200

    Comments.

--- builtin_custom/modules/test_modules.py
+++ builtin_custom/modules/test_modules.py
@@ -1,6 +1,19 @@
+""" # Importation """
+# import printNumbers
+
+""" # Renaming """
 import printNumbers as PN
-# from printNumbers import printBackwards # import a specific fonction
-# from printNumbers import printBackwards as pb # import a specific fonction and rename
+
+""" # import a specific fonction """
+# from printNumbers import printBackwards 
+
+""" import a specific fonction and rename """
+# from printNumbers import printBackwards as pb
+
+""" import all the names that a module defines there is another variant for importing. 
+This imports all names except those beginning with an underscore (_). 
+But this is not ideal practice as this introduces an unknown set of names into the interpreter. """
+# from printNumbers import * 
 
 PN.printForward(10)
 print("#"*15)
