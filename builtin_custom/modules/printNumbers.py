0d126096f79397ae8f90091e3476e981a31cd2e4
BillMick <ahouandjinoubill@gmail.com>
2025-07-27 22:45:18 +0200

    Modules importation.

---
+++ builtin_custom/modules/printNumbers.py
@@ -0,0 +1,10 @@
+def printForward(n):
+    #print 1 to n
+    for i in range(n):
+        print(i+1)
+
+
+def printBackwards(n):
+    #print n to 1
+    for i in range(n):
+        print(n-i)
---
+++ builtin_custom/modules/test_modules.py
@@ -0,0 +1,7 @@
+import printNumbers as PN
+# from printNumbers import printBackwards # import a specific fonction
+# from printNumbers import printBackwards as pb # import a specific fonction and rename
+
+PN.printForward(10)
+print("#"*15)
+PN.printBackwards(5)
