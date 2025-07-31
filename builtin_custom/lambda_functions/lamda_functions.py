0193c5ed99e1ff48f32a1bc8a86af1eec52732e8
BillMick <ahouandjinoubill@gmail.com>
2025-07-31 12:50:50 +0200

    anonymous function and HOF.

--- builtin_custom/lambda_functions/lamda_functions.py
+++ builtin_custom/lambda_functions/lamda_functions.py
@@ -34,4 +34,32 @@ print(f"8 au carré = {sqrt_it(8)}")
 # expressed in Python lambdas by listing arguments and separating them with 
 # a comma (,) but without surrounding them with parentheses:
 fullname = lambda first, last: f"Full name: {first.title()} {last.title()}"
-print(fullname('Bill', 'Mick'))
+print("Full Name:", fullname('Bill', 'Mick'))
+
+""" Anonymous Functions """
+""" Taken literally, an anonymous function is a function without a name. In Python, 
+an anonymous function is created with the lambda keyword. More loosely, it may or 
+not be assigned a name. """
+
+# lambda x, y: x + y
+# Other than providing you with the feedback that Python is perfectly fine with this 
+# form, it doesn’t lead to any practical use. You could invoke the function in the 
+# Python interpreter:
+# _(1, 2)
+""" The example commented above is taking advantage of the interactive interpreter-only feature provided via the underscore (_)
+https://dbader.org/blog/meaning-of-underscores-in-python """
+
+# Another pattern used in other languages like JavaScript is to immediately execute 
+# a Python lambda function. This is known as an Immediately Invoked Function 
+# Expression (IIFE, pronounce “iffy”). Here’s an example:
+(lambda x, y: x + y) (7, 8)
+""" Python does not encourage using immediately invoked lambda expressions. It 
+simply results from a lambda expression being callable, unlike the body of a 
+normal function. """
+
+""" Lambda functions are frequently used with higher-order functions, which take one 
+or more functions as arguments or return one or more functions.
+A lambda function can be a higher-order function by taking a function (normal or 
+lambda) as an argument like in the following contrived example: """
+high_ord_func = lambda x, func: x + func(x)
+print(f"HOF: {high_ord_func(2, lambda x: x * x)}")
